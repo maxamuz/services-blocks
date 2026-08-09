f651c49f662debe9d270c211802ff97f6ff412ff
Alexander <laptevx@dmail.com>
2026-08-09 17:49:59 +0300

    Стили h1

--- build/hero-block/index.asset.php
+++ build/hero-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '6314ff55246cd1c323c3');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '5624650e253e7486ce5a');
