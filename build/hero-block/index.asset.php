d9c340d213762fda1665194f9f7c0d8e6272c603
Alexander <laptevx@dmail.com>
2026-08-09 17:45:04 +0300

    Оставляем  CSS «значения напрямую» без глобальных стилей.

--- build/hero-block/index.asset.php
+++ build/hero-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'c993ab1c90be7f6b1eff');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '6314ff55246cd1c323c3');
