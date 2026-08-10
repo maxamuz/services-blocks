981774c8619b4a042eb90c0e7c0c77060b87b481
Alexander <laptevx@dmail.com>
2026-08-10 16:17:33 +0300

    Правка стилей hero блока

--- build/hero-block/index.asset.php
+++ build/hero-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'c96978c09a450191a26c');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'c8f86f798106310d1d7c');
